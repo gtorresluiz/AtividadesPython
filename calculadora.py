8ccba5d6cde12069bb8e26e9d384b115c7e7a175
Gabriel Torres <126830021+gtorresluiz@users.noreply.github.com>
2023-04-28 22:18:28 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,32 @@
+while True:
+
+    num1 = int(input("digite o primero numero: "))
+    num2 = int(input("digite o segundo numero: "))
+    op = str(input("qual operação deseja realizar entre eles? (+, -, *, /)"))
+    result = 0
+    valid = True
+
+    match op:
+        case '+':
+            op = 'soma'
+            result = num1 + num2
+        case '-':
+            op = 'subtração'
+            result = num1 - num2
+        case '/':
+            if num2 == 0:
+                print("não é possivel dividir por 0")
+                valid = False
+            else:
+                op = 'divisão'
+                result = num1 / num2
+        case '*':
+            op = 'multiplicação'
+            result = num1 * num2
+        case _:
+            print("escolha uma operação válida (+,-,*,/)")
+            valid = False
+    
+    if valid:
+        print("o resultado da %s entre %d e %d resulta em %d" %(op, num1, num2, result))
+        break
